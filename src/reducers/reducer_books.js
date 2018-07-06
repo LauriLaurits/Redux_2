9eb089a1a1b88975807525f8ce1295ab5efe6ffd
Lauri Laurits <lauri.laurits@adm.ee>
2018-07-06 11:51:35 +0300

    last adm not work

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Parts", pages:101 },
+    { title: "Harry Poter", pages:13 },
+    { title: "The Dark Tower", pages: 21 },
+    { title: "Eloquent Ruby", pages: 31 }
+  ];
+}
